c249612a3bbef817835deb6e88b1d7187314b8cf
Arkadiusz Choczaj <choczaj.arkadiusz@gmail.com>
2019-01-31 03:20:02 +0100

    add app startup method main

---
+++ src/main/java/pl/choczaj/spring/mobilerepair/MobilerepairApplication.java
@@ -0,0 +1,14 @@
+package pl.choczaj.spring.mobilerepair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilerepairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilerepairApplication.class, args);
+	}
+
+}
+
